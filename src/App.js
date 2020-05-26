e02bf8fdb9cc09eb13fe0717bde0f38325902b18
shahpreetk <preet25@somaiya.edu>
2020-05-26 15:20:28 +0530

    had forgotten to commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/HomePage/HomePage';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-     < HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
